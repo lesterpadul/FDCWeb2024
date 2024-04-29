f34ddf21d2aeb497578af3a5078425bdb8e45dca
Lester Padul <padullester@gmail.com>
2024-04-29 17:55:16 +0800

    Revert "NJMT-1: delete login"
    This reverts commit 697504b5cbaa3ff9a86e1a2c679d4301f4b6def0.

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- make a logo banner -->
+    <h1>My Website</h1>
+    
+    <!-- add an iframe that plays a youtube video: rick roll -->
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <!-- create a floating login screen -->
+    <form action="login.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
