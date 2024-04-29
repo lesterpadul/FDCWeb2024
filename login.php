4fc143b6a0b05beb215d029d4fb2cb4c1802f928
Lester Padul <padullester@gmail.com>
2024-04-29 18:29:17 +0800

    NJMT-3: add debug code

--- login.php
+++ login.php
@@ -11,6 +11,8 @@
     
     this is a legit change 
     
+    asdadasdasd
+    
     <!-- add an iframe that plays a youtube video: rick roll -->
     <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
