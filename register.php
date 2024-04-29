dff3e42757b20f252005594fc44d666e7bf84711
Lester Padul <padullester@gmail.com>
2024-04-29 17:30:32 +0800

    NJMT-1: add registration form

---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Form to register a new user -->
+    <form action="register.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="password" name="confirm_password" placeholder="Confirm Password">
+        <button type="submit">Register</button>
+    </form>    
+</body>
+</html>
